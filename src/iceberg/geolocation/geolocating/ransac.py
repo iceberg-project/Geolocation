dbe64d397b67d33f605227befb575285d0b48a46
AymenFJA <aymenfadhel@yahoo.com>
2020-02-05 12:46:36 -0500

    fixing ransac tabs

--- src/iceberg/geolocation/geolocating/ransac.py
+++ src/iceberg/geolocation/geolocating/ransac.py
@@ -40,7 +40,7 @@ class RansacFilter(object):
         self._publisher_in = Publisher(channel=self._name, url=self._in_addr_in)
         self._subscriber_in = Subscriber(channel=self._name, url=self._in_addr_out)
         self._subscriber_in.subscribe(topic=self._name)
-	toc = time.time()
+        toc = time.time()
         self._timings.loc[len(self._timings)] = ['configure',tic,toc,0]
     
 
@@ -49,8 +49,8 @@ class RansacFilter(object):
         self._publisher_in.put(topic='request', msg={'name': self._name,
                                                      'request': 'connect',
                                                      'type': 'receiver'})
-	toc = time.time()
-	self._timings.loc[len(self._timings)] = ['connect',tic,toc,0]
+        toc = time.time()
+        self._timings.loc[len(self._timings)] = ['connect',tic,toc,0]
 
 
     def _disconnect(self):
@@ -58,7 +58,7 @@ class RansacFilter(object):
         self._publisher_in.put(topic='request', msg={'name': self._name,
                                                      'type': 'receiver',
                                                      'request': 'disconnect'})
-	toc = time.time()
+        toc = time.time()
         self._timings.loc[len(self._timings)] = ['disconnect',tic,toc,0]
 
 
@@ -71,67 +71,64 @@ class RansacFilter(object):
 
         if recv_message[b'type'] == b'image':
             return recv_message[b'data']
-	    print (recv_message[b'data'])
+            print (recv_message[b'data'])
 
-	print ('message not received')
+        print ('message not received')
         return None
 
     
     def _ransac(self, img1, img2, matches):
 
 
-	# time it
+        # time it
         tic = time.time()
 
-	print ('This is Ransac matching function')
-	print (img1, img2, matches)
-	
-	#Trying stupid name techinque
-	base1=os.path.basename(img1)
-	base2=os.path.basename(img2)
-	name1=os.path.splitext(base1)[0]
-	name2=os.path.splitext(base2)[0]
+        print ('This is Ransac matching function')
+        print (img1, img2, matches)
+    
+        #Trying stupid name techinque
+        base1=os.path.basename(img1)
+        base2=os.path.basename(img2)
+        name1=os.path.splitext(base1)[0]
+        name2=os.path.splitext(base2)[0]
 
-	ransac_name = 'ransaced_matches_'+name1+'_'+name2+'.csv'
-	
-	#ransac_name = 'ransac.csv'
-	output_folder = "/pylon5/mc3bggp/aymen/ransac_out"+'/'+ransac_name
-	cmd = 'python /home/aymen/SummerRadical/4DGeolocation/ASIFT/src/PHASE_3_RANSAC_FILTERING/ransac_filter.py'
-	os.system(cmd+' -img1_filename '+img1+' -img1_nodata '+'0'+' -img2_filename '+img2+' -img2_nodata '+'0'+' '+matches+' '+output_folder)
-
-	count += 1
-	toc = time.time()
-	self._timings.loc[len(self._timings)] = [ransac_name,tic,toc,count]
+        ransac_name = 'ransaced_matches_'+name1+'_'+name2+'.csv'
+    
+        output_folder = "/pylon5/mc3bggp/aymen/ransac_out"+'/'+ransac_name
+        cmd = 'python /home/aymen/SummerRadical/4DGeolocation/ASIFT/src/PHASE_3_RANSAC_FILTERING/ransac_filter.py'
+        os.system(cmd+' -img1_filename '+img1+' -img1_nodata '+'0'+' -img2_filename '+img2+' -img2_nodata '+'0'+' '+matches+' '+output_folder)
+        count += 1
+        toc = time.time()
+        self._timings.loc[len(self._timings)] = [ransac_name,tic,toc,count]
 
     def run(self):
 
         self._connect()
-
         cont = True
-	count = 0
+        count = 0
 
         while cont:
             message = self._get_message()
             sys.stdout.flush()
-	    
+        
             if message not in ['disconnect','wait']:
                 try:
                     print(message)
-		    img1, img2, matches = message.split('$')
+                    img1, img2, matches = message.split('$')
                     sys.stdout.flush()
                     self._ransac(img1,img2,matches)
-		    print('Matches are filtered')
+                    print('Matches are filtered')
                 except:
                     sys.stdout.flush()
                     print('Matches are not filtered')
                     sys.stdout.flush()
             elif message == 'wait':
-                time.sleep(1)
+                 time.sleep(1)
             else:
                 self._disconnect()
                 cont = False
 
-	self._timings.to_csv(self._name + ".csv", index=False)
+        self._timings.to_csv(self._name + ".csv", index=False)
 
 
 if __name__ == "__main__":
